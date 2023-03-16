8e34a379e4125a5601c18c2882e2e3fb175f19d0
MMastromonaco <larssonmartin1306@gmail.com>
2023-03-16 15:36:23 +0100

    filterExpenses solved, historyDisplay removed

--- main.js
+++ main.js
@@ -42,13 +42,22 @@ Vue.createApp({
           result[category] += expense.amount;
         }
         return result;
-      }, {});
+      },
+        {});
     },
     remainingAmount() {
       if (this.salary === null) {
         return null
       }
       return this.salary - this.totalAmount
+    },
+    filterExpenses() {
+      // return this.expenses.filter(e => e.amount > 0);
+      const selectedMonth = this.expenseMonth;
+      return this.expenses.filter(e => {
+        const expenseDate = new Date(e.date);
+        return expenseDate.getMonth() == selectedMonth;
+      });
     }
   },
   methods: {
@@ -57,14 +66,6 @@ Vue.createApp({
     },
     removeNumber() {
       this.sumString = "";
-    },
-    historyDisplay() {
-      for (let expense of this.expenses) {
-        let validDate = new Date(expense.date);
-        if (validDate.getMonth() == this.expenseMonth) {
-          console.log(expense.date, expense.category, expense.amount)
-        }
-      }
     },
     addExpense() {
       if (!this.sumString || !this.expenseCategory) {
