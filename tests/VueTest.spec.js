45c04120c49b24bca0a66e0f7f8392d678f004a6
MMastromonaco <larssonmartin1306@gmail.com>
2023-03-23 14:00:31 +0100

    snälla

--- tests/VueTest.spec.js
+++ tests/VueTest.spec.js
@@ -1,45 +1,9 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-const new_Expense = {
-  amount: 1500,
-  category: "stocks",
-  name: "Ehang"
-}
-
 test('Add a expense', async ({ page }) => {
   await page.goto('http://127.0.0.1:5500/');
 
-<<<<<<< HEAD
-  // Enter expense details
-  await page.fill('input[id="overlay"]', '50');
-  await page.selectOption('select[id="expenseCategory"]', 'Food');
-  await page.fill('input[id="objectName"]', 'Ice cream');
-
-  // Submit the form
-  await page.click('button[type="submit"]');
-
-  // Expect to see the new expense in the expenses list
-  expect(await page.isVisible('ul > li:last-child')).toBeTruthy();
-});
-
-test('delete a expense', async ({ page }) => {
-  await page.goto('http://127.0.0.1:5500/');
-
-  await Promise.all([
-    page.click('.expand-button')
-  ]);
-
-  const deleteButton = await page.$('#delete-transaction-1')
-  await deleteButton?.click();
-
-  // Submit the delete
-  await page.click('button[id="deleteButton"]');
-
-  const transaction = await page.$('#transaction-1');
-  expect(transaction).toBeNull();
-});
-=======
     const addAmount = page.locator('#overlay');
     const selectCategory = page.locator('#expenseCategory');
     const addName = page.locator('#objectName')
@@ -125,4 +89,3 @@ test('Delete transaction', async ({ page }) => {
 
     await expect(trackerText).toEqual('Total amount: 100.00');
 });
->>>>>>> 5d6c95cf699b3be84a95994963f761d8abe7c4c8
